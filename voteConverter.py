ecbc6922bff1fd12fee6623f8c44a5c6fa6cd4a2
jwilderm <johannes.wildermuth@hsr.ch>
2019-09-26 16:52:55 +0200

    Refactoring: Extract method

--- voteConverter.py
+++ voteConverter.py
@@ -1,6 +1,31 @@
 import xml.etree.ElementTree as ET
 import sys
 
+def node_to_nwiki(node):
+    if node.text is not None:
+        if node.tag == 'QuestionText':
+            print(f'={node.text}=')
+        elif node.tag == 'Answer1':
+            print(f'==A: {node.text}==')
+        elif node.tag == 'Answer2':
+            print(f'==B: {node.text}==')
+        elif node.tag == 'Answer3':
+            print(f'==C: {node.text}==')
+        elif node.tag == 'Answer4':
+            print(f'==D: {node.text}==')
+        elif node.tag == 'PictureQuestionText':
+            print(f'[[image:{node.text}|{node.text}]]')
+        elif node.tag == 'PictureAnswer1':
+            print(f'==A:==\n[[image:{node.text}|{node.text}]]')
+        elif node.tag == 'PictureAnswer2':
+            print(f'==B:==\n[[image:{node.text}|{node.text}]]')
+        elif node.tag == 'PictureAnswer3':
+            print(f'==C:==\n[[image:{node.text}|{node.text}]]')
+        elif  node.tag == 'PictureAnswer4':
+            print(f'==D:==\n[[image:{node.text}|{node.text}]]')
+        elif node.tag == 'Solution':
+            print(f"'''Solution: {node.text}'''")
+
 #xmlTree = ET.parse(sys.argv[1])
 xmlTree = ET.parse('BspFragen.xml') #for testing
 root = xmlTree.getroot()
@@ -9,30 +34,4 @@ assert root.tag == 'ArrayOfQuestion'
 for question in root:
     assert question.tag == 'Question'
     for node in question:
-        if node.text is not None:
-            if node.tag == 'QuestionText':
-                print(f'={node.text}=')
-            elif node.tag == 'Answer1':
-                print(f'==A: {node.text}==')
-            elif node.tag == 'Answer2':
-                print(f'==B: {node.text}==')
-            elif node.tag == 'Answer3':
-                print(f'==C: {node.text}==')
-            elif node.tag == 'Answer4':
-                print(f'==D: {node.text}==')
-            elif node.tag == 'PictureQuestionText':
-                print(f'[[image:{node.text}|{node.text}]]')
-            elif node.tag == 'PictureAnswer1':
-                print('==A:==')
-                print(f'[[image:{node.text}|{node.text}]]')
-            elif node.tag == 'PictureAnswer2':
-                print('==B:==')
-                print(f'[[image:{node.text}|{node.text}]]')
-            elif node.tag == 'PictureAnswer3':
-                print('==C:==')
-                print(f'[[image:{node.text}|{node.text}]]')
-            elif  node.tag == 'PictureAnswer4':
-                print('==D:==')
-                print(f'[[image:{node.text}|{node.text}]]')
-            elif node.tag == 'Solution':
-                print(f"'''Solution: {node.text}'''")
+        node_to_nwiki(node)
