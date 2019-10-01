93b5f0c54a4b48b72ce4f32c49df75990724ab6a
jwilderm <johannes.wildermuth@hsr.ch>
2019-10-01 09:30:04 +0200

    Fix: Works now for both python versions 2 and 3

--- voteConverter.py
+++ voteConverter.py
@@ -16,14 +16,25 @@ nwiki_snippets = {
     'Solution': "'''Solution: {node_text}'''"
     }
 
+# Gets the text from the accepted nodes
 def node_to_nwiki(node):
     if node.text is not None:
         nwiki_snippet = nwiki_snippets.get(node.tag)
         if nwiki_snippet is not None:
-            return nwiki_snippet.format(node_text=node.text)
+            if is_version_old:
+                node_text = node.text.encode('utf-8')
+            else:
+                node_text = node.text
+            return nwiki_snippet.format(node_text=node_text)
+
+# Check if version is below 3
+is_version_old = sys.version_info[0] < 3
 
 # Parses a xml-file with the given name
-xml_name = input('The name of your xml-file: ')
+if is_version_old:
+    xml_name = raw_input('The name of your xml-file: ')
+else:
+    xml_name = input('The name of your xml-file: ')
 xmlTree = ET.parse(xml_name)
 file_name = xml_name.split('.')[0] + '.txt'
 root = xmlTree.getroot()
@@ -35,6 +46,5 @@ with open(file_name, 'w') as w:
         assert question.tag == 'Question'
         for node in question:
             nwiki_snippet = node_to_nwiki(node)
-
             if nwiki_snippet is not None:
                 w.write(nwiki_snippet + '\n')
